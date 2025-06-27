6b3de5ebb50a95cfcc38c4bdb0b64e13a2e4d98a
Tharunkumar <tharunkumar10406@gmail.com>
2025-06-27 06:55:42 +0530

    server update

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ app.use(cors({
 app.use(express.json());
 
 // ✅ MongoDB Connection
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URL)
+
 })
     .then(() => console.log('✅ MongoDB connected'))
     .catch((err) => console.error("❌ MongoDB connection error:", err.message));
